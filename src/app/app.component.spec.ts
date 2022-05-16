bd7f5a55c5df838c749d2ba2c70ea1f26c0c0f8c
Chris Larry <master@Chriss-MacBook-Air.local>
2022-05-15 19:50:12 -0700

    STAAR 3-5

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-starter'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-starter');
+    expect(app.title).toEqual('More Problems');
   });
 
   it('should render title', () => {
