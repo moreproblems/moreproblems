47bd90f7a3b151689b07675fb7f065e05d1dd928
Chris Larry <master@Chriss-Air.attlocal.net>
2022-08-29 17:17:42 -0700

    Top 10 state exams; About Us tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ExamsComponent } from './components/exams/exams.component';
+import { ProblemsComponent } from './components/problems/problems.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExamsComponent,
+    ProblemsComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
