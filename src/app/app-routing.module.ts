45699a7aa7e5c59b4ef0f299b828e27908562d6d
Chris Larry <master@Chriss-Air.attlocal.net>
2022-09-07 21:25:48 -0700

    gtag

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ExamsComponent } from './components/exams/exams.component';
-// import { ProblemsComponent } from './components/problems/problems.component';
+import { ProblemsComponent } from './components/problems/problems.component';
 import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'about', pathMatch: 'full' },
   { path: 'exams', component: ExamsComponent },
-  // { path: 'problems', component: ProblemsComponent },
+  { path: 'problems', component: ProblemsComponent },
   { path: 'about', component: AboutComponent }
 ];
 
