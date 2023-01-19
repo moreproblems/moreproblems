7180fed3fdd0067f107ff562cc5bcf4897b425f7
Chris Larry <chrislarry@Chriss-Air.attlocal.net>
2023-01-18 16:57:54 -0800

    exam fixes

--- src/app/components/standards/standards.component.ts
+++ src/app/components/standards/standards.component.ts
@@ -212,7 +212,7 @@ export class StandardsComponent implements OnInit{
   // }
 
   ngOnInit() {
-    this.titleService.setTitle("MoreProblems.Org | U.S. K-12 State Testing Practice Exams");
-    this.meta.updateTag({ name: 'description', content: "Access released practice problems & solutions to prepare for end-of-year tests - including Florida FSA, Illinois IAR, New York NYSTP, North Carolina EOG, Pennsylvania PSSA, and Texas STAAR. Choose from more 300 assessments across math, English language, science, & social studies for elementary, middle, & high school students." });
+    this.titleService.setTitle("MoreProblems.Org | U.S. K-12 Common Core Learning Stanadards");
+    this.meta.updateTag({ name: 'description', content: "Find out what to expect from your learner's curriculum, all the way down to standards. Subjects include Math & English Language Arts from Kindergarten through High School - as they are outlined by the Common Core state standards adopted by most states in America." });
   }
 }
