bcb41323343f3f4a18b94a51c5c7360d5fe8dddf
Chris Larry <chrislarry@Chriss-Air.attlocal.net>
2023-07-14 18:02:42 -0700

    TX21G6M

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -48,6 +48,8 @@ import { TX19G5SExamComponent } from './components/TX19G5S-exam/TX19G5S-exam.com
 import { TX19G5SKeyComponent } from './components/TX19G5S-key/TX19G5S-key.component';
 import { TX18G5SExamComponent } from './components/TX18G5S-exam/TX18G5S-exam.component';
 import { TX18G5SKeyComponent } from './components/TX18G5S-key/TX18G5S-key.component';
+import { TX21G6MExamComponent } from './components/TX21G6M-exam/TX21G6M-exam.component';
+import { TX21G6MKeyComponent } from './components/TX21G6M-key/TX21G6M-key.component';
 import { TX22G8SExamComponent } from './components/TX22G8S-exam/TX22G8S-exam.component';
 import { TX22G8SKeyComponent } from './components/TX22G8S-key/TX22G8S-key.component';
 import { TX21G8SExamComponent } from './components/TX21G8S-exam/TX21G8S-exam.component';
@@ -134,6 +136,8 @@ const routes: Routes = [
   { path: 'exam/TX19G5S/key', component: TX19G5SKeyComponent },
   { path: 'exam/TX18G5S', component: TX18G5SExamComponent },
   { path: 'exam/TX18G5S/key', component: TX18G5SKeyComponent },
+  { path: 'exam/TX21G6M', component: TX21G6MExamComponent },
+  { path: 'exam/TX21G6M/key', component: TX21G6MKeyComponent },
   { path: 'exam/TX22G8S', component: TX22G8SExamComponent },
   { path: 'exam/TX22G8S/key', component: TX22G8SKeyComponent },
   { path: 'exam/TX21G8S', component: TX21G8SExamComponent },
