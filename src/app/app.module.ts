349877130bed0be11bb00472fc4cbc8db12f2f82
Chris Larry <chrislarry@Chriss-Air.attlocal.net>
2023-07-18 11:33:00 -0700

    TX19G7M

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -99,6 +99,8 @@ import { TX19G6MExamComponent } from './components/TX19G6M-exam/TX19G6M-exam.com
 import { TX19G6MKeyComponent } from './components/TX19G6M-key/TX19G6M-key.component';
 import { TX21G7MExamComponent } from './components/TX21G7M-exam/TX21G7M-exam.component';
 import { TX21G7MKeyComponent } from './components/TX21G7M-key/TX21G7M-key.component';
+import { TX19G7MExamComponent } from './components/TX19G7M-exam/TX19G7M-exam.component';
+import { TX19G7MKeyComponent } from './components/TX19G7M-key/TX19G7M-key.component';
 import { TX22G8SExamComponent } from './components/TX22G8S-exam/TX22G8S-exam.component';
 import { TX22G8SKeyComponent } from './components/TX22G8S-key/TX22G8S-key.component';
 import { TX21G8SExamComponent } from './components/TX21G8S-exam/TX21G8S-exam.component';
@@ -207,6 +209,8 @@ import { TX18G8SSKeyComponent } from './components/TX18G8SS-key/TX18G8SS-key.com
     TX19G6MKeyComponent,
     TX21G7MExamComponent,
     TX21G7MKeyComponent,
+    TX19G7MExamComponent,
+    TX19G7MKeyComponent,
     TX22G8SExamComponent,
     TX22G8SKeyComponent,
     TX21G8SExamComponent,
