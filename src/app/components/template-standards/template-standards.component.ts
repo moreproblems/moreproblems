1126e1b4e45d6ce058b631c03e525b435a4225ca
moreproblems <chriscolay1@gmail.com>
2025-03-22 10:18:11 -0700

    MFR update

--- src/app/components/template-standards/template-standards.component.ts
+++ src/app/components/template-standards/template-standards.component.ts
@@ -1701,14 +1701,21 @@ export class TemplateStandardsComponent implements OnInit {
     }
 
     attempt_mfr_st_problem(choice: string, inum: string, part: string) {
-        var correct: boolean = false;
+        var correct: boolean = true;
+        var attempt: boolean = false;
         var part_num = 0;
         if (part != '') {
             part_num = Object.keys(this.subtopic_search_dump[this.subtopic_problem_number].Parts).indexOf(part);
         }
         if (choice != this.subtopic_problem_selection[part_num][+inum - 1]) {
-            this.subtopic_problem_attempts[part_num] += 1;
+            if (this.subtopic_problem_selection[part_num][+inum - 1] != '') {
+                this.subtopic_problem_attempts[part_num] += 1;
+                attempt = true;
+            }
             this.subtopic_problem_selection[part_num][+inum - 1] = choice;
+            if (!attempt && !this.subtopic_problem_selection[part_num].includes('')) {
+                this.subtopic_problem_attempts[part_num] += 1;
+            }
             this.subtopic_attempt_path[part_num].push(this.subtopic_problem_selection[part_num]);
             this.subtopic_attempt_response[part_num] = '';
             for (const [num, prob] of Object.entries(this.subtopic_search_dump)) {
@@ -1716,16 +1723,26 @@ export class TemplateStandardsComponent implements OnInit {
                     if (part == '') {
                         for (const [ch, key] of Object.entries(prob.AnswerChoices)) {
                             if (inum + ':KEY' == ch && choice == key.Choice) {
-                                correct = true;
                                 this.subtopic_attempt_explanation[part_num][+inum - 1] = key.Key.Rationale;
+                            }
+                            else if (inum + ':KEY' == ch && choice != key.Choice) {
+                                correct = false;
+                            }
+                            else if (this.subtopic_problem_selection[part_num][+ch[0]-1] != key.Choice) {
+                                correct = false;
                             }
                         }
                     }
                     else {
                         for (const [ch, key] of Object.entries(prob.Parts[part].AnswerChoices)) {
                             if (inum + ':KEY' == ch && choice == key.Choice) {
-                                correct = true;
                                 this.subtopic_attempt_explanation[part_num][+inum - 1] = key.Key.Rationale;
+                            }
+                            else if (inum + ':KEY' == ch && choice != key.Choice) {
+                                correct = false;
+                            }
+                            else if (this.subtopic_problem_selection[part_num][+ch[0]-1] != key.Choice) {
+                                correct = false;
                             }
                         }
                     }
@@ -2456,7 +2473,7 @@ export class TemplateStandardsComponent implements OnInit {
         if (part != '') {
             part_num = Object.keys(this.subtopic_search_dump[this.subtopic_problem_number].Parts).indexOf(part);
         }
-        const TIel: string = "inputT-" + part + '-' + index;
+        const TIel: string = "inputTST-" + part + '-' + index;
         var input: any = document.getElementById(TIel);
         return input.value;
     }
@@ -2466,7 +2483,7 @@ export class TemplateStandardsComponent implements OnInit {
         if (part != '') {
             part_num = Object.keys(this.subtopic_search_dump[this.subtopic_problem_number].Parts).indexOf(part);
         }
-        const MFRIel: string = "inputMFR-" + part + "-" + index;
+        const MFRIel: string = "inputMFRST-" + part + "-" + index;
         var dropdown: any = document.getElementById(MFRIel);
         return dropdown.value;
     }
